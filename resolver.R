40f74bb79ee124bb71a09dd798e0533ac53f716e
Federico Domínguez <fexadom@gmail.com>
2021-02-19 11:10:49 +0100

    Nuevo resolver.R

--- resolver.R
+++ resolver.R
@@ -2,13 +2,12 @@ overbooking <- 18
 asientos <- 180
 n <- overbooking + asientos
 p <- 0.9 #probabilidad abordar el avión
-x <- seq(0,n)
+x <- seq(150,n)
 y <- dbinom(x,n,p)
 plot(x,y,col="blue", xlab="pasajeros abordando", ylab="probabilidad")
+abline(v=asientos,col="red")
 
-#Aproximación normal
-m <- n*p #aproximación promedio
-s <- sqrt(n*p*(1-p)) #aproximación desviación estandar
-
-yn <- dnorm(x, mean = m, sd = s)
-lines(x,yn,type="l",col="red")
+#TODO
+#Demostrar que 198 es la sobreventa ideal con un gráfico que muestre:
+#Asientos vendidos (eje X) vs Revenue (eje Y) en donde se resalte usando
+#una linea vertical que el pico de ganancia (Revenue) se encuentra en 198
